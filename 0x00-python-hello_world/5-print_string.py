e30e4df16bec3e4c77b665bb93bc74db33406e24
SantiagoHerreG <888@holbertonschool.com>
2019-09-09 18:32:19 +0000

    5: script that prints a string 4 times, correction of format

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Holberton School"
-print("{}{}{}".format(str, str, str))
-print("{}".format(str[:10]))
+print("{0:s}{1:s}{2:s}".format(str, str, str))
+print("{:s}".format(str[:10]))
