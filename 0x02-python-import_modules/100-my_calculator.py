e3269cd2532b64dd6532516bd9f0cd9dca646480
SantiagoHerreG <888@holbertonschool.com>
2019-09-12 19:12:40 +0000

    100: calculator in command line

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    arg_c = int(len(sys.argv))
+    if arg_c != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    if (sys.argv[2] in ("+", "-", "/", "*")) is False:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    term1 = int(sys.argv[1])
+    term2 = int(sys.argv[3])
+    if sys.argv[2] == "+":
+        result = term1 + term2
+    elif sys.argv[2] == "-":
+        result = term1 - term2
+    elif sys.argv[2] == "*":
+        result = term1 * term2
+    else:
+        result = term1 / term2
+    print("{} {} {} = {}".format(term1, sys.argv[2], term2, result))
