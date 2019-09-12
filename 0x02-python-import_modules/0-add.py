5fbfcd355c419e8f7881b6da9066bc8184f32d8f
SantiagoHerreG <888@holbertonschool.com>
2019-09-12 17:06:33 +0000

    0: using import to use a function from a module in a file and using __name__ and __main__ to not execute the script when called as __import__, correction of importing

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import add_0
+    from add_0 import add
     a = 1
     b = 2
-    c = add_0.add(a, b)
+    c = add(a, b)
     print("{0:d} + {1:d} = {2:d}".format(a, b, c))
