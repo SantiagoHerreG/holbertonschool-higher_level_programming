03020d7960b8caa4633e648659bc1fbb48350af5
SantiagoHerreG <888@holbertonschool.com>
2019-09-12 18:24:04 +0000

    4: print a list of names (using dir() funciton) in a compiled python script

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
 
-import hidden_4
+    name_list = dir(hidden_4)
 
-name_list = dir(hidden_4)
-
-for i in range(0, len(name_list)):
-    if (name_list[i][0] == "_" and name_list[i][1] == "_"):
-        continue
-    print(name_list[i])
+    for i in range(0, len(name_list)):
+        if (name_list[i][0] == "_" and name_list[i][1] == "_"):
+            continue
+        print(name_list[i])
