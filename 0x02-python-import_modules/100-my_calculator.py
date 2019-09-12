5056aad89fa06f2e8569fc173d6cdfc41ddda1b9
SantiagoHerreG <888@holbertonschool.com>
2019-09-12 23:01:17 +0000

    100: basic calculator in command line, edge case handled, corr

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,24 +1,30 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+
     import sys
+
     arg_c = int(len(sys.argv))
+
     if arg_c != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     if (sys.argv[2] in ("+", "-", "/", "*")) is False:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
+
     term1 = int(sys.argv[1])
     term2 = int(sys.argv[3])
+
     if sys.argv[2] == "+":
-        result = term1 + term2
+        res = term1 + term2
     elif sys.argv[2] == "-":
-        result = term1 - term2
+        res = term1 - term2
     elif sys.argv[2] == "*":
-        result = term1 * term2
+        res = term1 * term2
     else:
         if term2 == 0:
-            print("Division is not possible")
+            print("{0:d} / {1:d} = Infinite".format(term1, term2))
             exit(0)
-        result = term1 / term2
-    print("{} {} {} = {}".format(term1, sys.argv[2], term2, result))
+        res = term1 / term2
+
+    print("{0:d} {1:s} {2:d} = {3:d}".format(term1, sys.argv[2], term2, res))
