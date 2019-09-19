f52d7ed25cec5babe8ed89e6cadb0aa0ac9b0d4c
SantiagoHerreG <888@holbertonschool.com>
2019-09-19 21:33:21 +0000

    12-roman_to_int: function that converts a string containing a number in romanic system to integers

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+
+    my_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+    new_list = []
+
+    for idx in range(len(roman_string)):
+        for elem in my_dict:
+            if roman_string[idx] == elem:
+                new_list.append(my_dict[elem])
+
+    count = 0
+
+    for i in range(len(new_list)):
+        if ((i + 1) == len(new_list) or new_list[i] >= new_list[i + 1]):
+            count += new_list[i]
+        else:
+            count -= new_list[i]
+
+    return (count)
