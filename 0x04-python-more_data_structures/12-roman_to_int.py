5e63b24f08b149835aa9c515b35c1f1bdd0fde73
SantiagoHerreG <888@holbertonschool.com>
2019-09-19 21:58:50 +0000

    12-roman_to_int: function that converts a string containing a number in romanic system to integer, output and edge case correction, isinstance funct added

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,7 +4,7 @@ def roman_to_int(roman_string):
     my_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     new_list = []
 
-    if not roman_string:
+    if (not roman_string or not isinstance(roman_string, str)):
         return (0)
 
     for idx in range(len(roman_string)):
