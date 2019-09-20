d72d21db3c5e00028e2817eca99bb3082f569f82
SantiagoHerreG <888@holbertonschool.com>
2019-09-20 03:33:23 +0000

    101-square: returns a 2d matrix with all values from orginal matrix to the square power

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return ([list(map(lambda x: x ** 2, i)) for i in matrix])
+    return(list(map(lambda x: list(map(lambda y: x[y] ** 2, range(len(x)))), matrix,)))
