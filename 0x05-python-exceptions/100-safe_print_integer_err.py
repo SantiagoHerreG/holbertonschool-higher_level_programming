5462877dab79e96ca6e42f49096aa92993f26090
SantiagoHerreG <888@holbertonschool.com>
2019-09-30 18:49:30 +0000

    100: Funtion that prints an integer, returns false on error and prints the error in stder, Excepton handling

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,5 +6,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except Exception as error:
-        print(error, file=sys.stderr)
+        print("Exception:", error, file=sys.stderr)
         return False
