8b6b46056cc5d2c876331128950bc8eb2f9af5d4
SantiagoHerreG <888@holbertonschool.com>
2019-10-01 21:23:10 +0000

    1-square: class that defines a square and an instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class Square:
+    """Empty class that defines a square.
+    """
+    def __init__(self, size):
+        """ __init__ method for the class.
+
+        Args:
+            size (int): size of the square side.
+        """
+        self.__size = size
