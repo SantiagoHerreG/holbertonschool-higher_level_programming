d9f9b302a69a784ced498290854d86ead3c5c222
SantiagoHerreG <888@holbertonschool.com>
2019-10-01 21:40:01 +0000

    3-square: class that creates a square, instance method for area added

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+class Square:
+    """Class that defines a square.
+    """
+    def __init__(self, size=0):
+        """ __init__ method for the class.
+
+        An error is raise in case the arg is not an int or less than 0.
+
+        Args:
+            size (int): size of the square side.
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise TypeError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Class method for area calculation.
+
+        Returns:
+            The area of th square..
+        """
+        return self.__size ** 2
