c8fd396e066d2f072467564ac483e306ef31a20b
SantiagoHerreG <888@holbertonschool.com>
2019-10-02 04:51:51 +0000

    Corrections in pep8 and google style

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -2,20 +2,20 @@
 import math
 
 
-def __init__(self, radius=0):
-    """Dissasembled from python bytecode.
-    Args:
-        radius (str): Description of radius.
-    """
-    self._MagicClass__radius = 0
-    if (type(radius) is not int and type(radius) is not float):
-        raise TypeError("radius must be a number")
-    self._MagicClass__radius = radius
+class MagicClass:
+    def __init__(self, radius=0):
+        """Dissasembled from python bytecode.
+        Args:
+            radius (str): Description of radius."""
+        self._MagicClass__radius = 0
+        if (type(radius) is not int and type(radius) is not float):
+            raise TypeError("radius must be a number")
+        self._MagicClass__radius = radius
 
+    def area(self):
 
-def area(self):
-    return (self._MagicClass__radius ** 2) * math.pi
+        return (self._MagicClass__radius ** 2) * math.pi
 
+    def circumference(self):
 
-def circumference(self):
-    return 2 * math.pi * self._MagicClass__radius
+        return 2 * math.pi * self._MagicClass__radius
