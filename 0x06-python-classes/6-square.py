084df1225bf6b9450cbe3e16b2823e9082c05cc7
SantiagoHerreG <888@holbertonschool.com>
2019-10-02 23:36:26 +0000

    6-square: class that creates a square and saves a position into a tuple for printing purposes

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -21,9 +21,11 @@ class Square:
 
         if type(position) is not tuple:
             raise TypeError("position must be a tuple of 2 positive integers")
-        elif ((type(position[0]) is not int) or position[0] < 0):
+        elif (position[0] is None or position[0] is not int):
             raise TypeError("position must be a tuple of 2 positive integers")
-        elif ((type(position[1]) is not int) or position[1] < 0):
+        elif (position[0] is None or position[1] is not int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif (position[0] < 0 or position[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = position
@@ -58,9 +60,11 @@ class Square:
     def position(self, value):
         if type(value) is not tuple:
             raise TypeError("position must be a tuple of 2 positive integers")
-        elif ((type(value[0]) is not int) or value[0] < 0):
+        elif (value[0] is None or value[0] is not int):
             raise TypeError("position must be a tuple of 2 positive integers")
-        elif ((type(value[1]) is not int) or value[1] < 0):
+        elif (value[0] is None or value[1] is not int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif (value[0] < 0 or value[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
