e32c2a993e5025446bc21e39c82dff346156ae0d
SantiagoHerreG <888@holbertonschool.com>
2019-10-02 04:44:52 +0000

    Corrections in pep8 and google style

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 import math
+"""int: Module level variable documented inline."""
 
 
 class Circle:
+    """The summary line for a class docstring should fit on one line."""
     def __init__(self, radius=0):
-        """Dissasembled from python bytecode.
-        Args:
-            radius (str): constant number. """
+        """Dissasembled from python bytecode. """
         self._MagicClass__radius = 0
         if (type(radius) is not int and type(radius) is not float):
             raise TypeError("radius must be a number")
