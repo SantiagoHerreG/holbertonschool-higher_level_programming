6d8b551f5fdd9fc91255adcadea8740ccb13382c
SantiagoHerreG <888@holbertonschool.com>
2019-10-02 04:49:18 +0000

    Corrections in pep8 and google style

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 import math
-"""int: Module level variable documented inline."""
 
 
-class Circle:
-    """The summary line for a class docstring should fit on one line."""
-    def __init__(self, radius=0):
-        """Dissasembled from python bytecode. """
-        self._MagicClass__radius = 0
-        if (type(radius) is not int and type(radius) is not float):
-            raise TypeError("radius must be a number")
-        self._MagicClass__radius = radius
+def __init__(self, radius=0):
+    """Dissasembled from python bytecode.
+    Args:
+        radius (str): Description of radius.
+    """
+    self._MagicClass__radius = 0
+    if (type(radius) is not int and type(radius) is not float):
+        raise TypeError("radius must be a number")
+    self._MagicClass__radius = radius
 
-    def area(self):
-        """Returns the area of a circle."""
-        return (self._MagicClass__radius ** 2) * math.pi
 
-    def circumference(self):
-        """Returns the circumference length of a circle."""
-        return 2 * math.pi * self._MagicClass__radius
+def area(self):
+    return (self._MagicClass__radius ** 2) * math.pi
+
+
+def circumference(self):
+    return 2 * math.pi * self._MagicClass__radius
