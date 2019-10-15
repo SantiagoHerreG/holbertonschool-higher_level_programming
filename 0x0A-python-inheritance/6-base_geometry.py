40b828e3c589ffe2cd5554fdbfa523777d6e2e93
SantiagoHerreG <888@holbertonschool.com>
2019-10-15 20:15:24 +0000

    6-bse_geometry: class with area method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Class BaseGeometry
+
+"""
+
+
+class BaseGeometry:
+    """ New class"""
+    def area(self):
+        """ Raises an error
+        """
+        raise Exception("area() is not implemented")
