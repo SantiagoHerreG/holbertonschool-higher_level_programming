a6e7b3f4cefa2c849bc537e0eaca36c1de1958ef
SantiagoHerreG <888@holbertonschool.com>
2019-10-16 13:45:53 +0000

    4-inherits_from: returns true if an instance is an instance of a subclass of another class

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Module with a function that returns True
+""" Function that returns True
 if the object is an instance of a class that inherited (directly or indirectly)
 from the specified class ; otherwise False"""
 
