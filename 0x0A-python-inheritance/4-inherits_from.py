0a1045879730af1f29a6cdcd2b2a0e1ce4ff4815
SantiagoHerreG <888@holbertonschool.com>
2019-10-16 13:50:21 +0000

    4-inherits_from: returns true if an instance is an instance of a subclass of another class

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-""" Function that returns True
-if the object is an instance of a class that inherited (directly or indirectly)
-from the specified class ; otherwise False"""
-
-
 def inherits_from(obj, a_class):
     """ Checks if obj is an instance of subclass of a_class
 
     """
     if type(obj) is not a_class and isinstance(obj, a_class):
         return True
-    return False
+    else:
+        return False
