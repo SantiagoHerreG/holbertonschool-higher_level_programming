3dab15bf11894523abbdcf48038d5cad3781b8df
SantiagoHerreG <888@holbertonschool.com>
2019-10-16 22:52:23 +0000

    101-stats: script that reads from stdin a series of lines and prints the statistics of lines read

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Script that reads stdin line by line and computes metrics
+"""
+
+
+import sys
+
+status_list = [0, 0, 0, 0, 0, 0, 0, 0]
+status_names = ["200", "301", "400", "401", "403", "404", "405", "500"]
+
+count = 0
+file_size = 0
+try:
+    for lines in sys.stdin:
+        count += 1
+        word_list = lines.split()
+        for i in range(len(status_names)):
+            if word_list[-2] == status_names[i]:
+                status_list[i] += 1
+        file_size += int(word_list[-1])
+        if count % 10 == 0:
+            print("File size: {}".format(file_size))
+            for i in range(len(status_list)):
+                if status_list[i]:
+                    print("{}: {}".format(status_names[i], status_list[i]))
+except KeyboardInterrupt:
+    print("File size: {}".format(file_size))
+    for i in range(len(status_list)):
+        if status_list[i]:
+            print("{}: {}".format(status_names[i], status_list[i]))
+    raise
