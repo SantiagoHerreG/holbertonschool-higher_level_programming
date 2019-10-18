ac204f679a7860b472770d499a5a4353b6518429
SantiagoHerreG <888@holbertonschool.com>
2019-10-18 04:36:15 +0000

    101-stats: script that reads from stdin a series of lines and prints the statistics of lines read

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -6,33 +6,33 @@
 import sys
 
 status_list = [0, 0, 0, 0, 0, 0, 0, 0]
-status_names = ["200", "301", "400", "401", "403", "404", "405", "500"]
+names = ["200", "301", "400", "401", "403", "404", "405", "500"]
 
 count = 0
 file_size = 0
+if not sys.stdin:
+    print("File size: {}".format(file_size))
 try:
     for lines in sys.stdin:
-        count += 1
         word_list = lines.split()
-        for i in range(len(status_names)):
-            if word_list[-2] == status_names[i]:
-                status_list[i] += 1
-        file_size += int(word_list[-1])
+        if len(word_list) >= 2:
+            for i in range(len(names)):
+                if names[i] in word_list and word_list[-1] != names[i]:
+                    status_list[i] += 1
+            file_size += int(word_list[-1])
+            count += 1
         if count % 10 == 0:
             print("File size: {}".format(file_size))
             for i in range(len(status_list)):
                 if status_list[i]:
-                    print("{}: {}".format(status_names[i], status_list[i]))
+                    print("{}: {}".format(names[i], status_list[i]))
 except KeyboardInterrupt:
     print("File size: {}".format(file_size))
     for i in range(len(status_list)):
         if status_list[i]:
-            print("{}: {}".format(status_names[i], status_list[i]))
+            print("{}: {}".format(names[i], status_list[i]))
     raise
-if file_size:
-    print("File size: {}".format(file_size))
-    for i in range(len(status_list)):
-        if status_list[i]:
-            print("{}: {}".format(status_names[i], status_list[i]))
-else:
-    print("File size: {}".format(file_size))
+print("File size: {}".format(file_size))
+for i in range(len(status_list)):
+    if status_list[i]:
+        print("{}: {}".format(names[i], status_list[i]))
