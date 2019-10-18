22bac5b4fd2414731122f90c6c8327adb1015d2f
SantiagoHerreG <888@holbertonschool.com>
2019-10-18 05:32:21 +0000

    Import of superclass added

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -4,7 +4,7 @@ Subclass Square
 Class BaseGoemetry
 """
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
-Rectangle = __import__('8-rectangle').Rectangle
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
