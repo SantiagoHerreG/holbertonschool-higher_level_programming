04e201b3b23200b0af5ad4f0db46b8c3ede6772a
SantiagoHerreG <888@holbertonschool.com>
2019-10-22 21:02:19 +0000

    square.py module updated to validate the width

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -26,6 +26,10 @@ class Square(Rectangle):
     @size.setter
     def size(self, value):
 
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.width = value
         self.height = value
         self.__size = value
