04d488240b51f5a4ae90082fd60bfbb656c2cf91
SantiagoHerreG <888@holbertonschool.com>
2019-11-26 21:29:14 +0000

    0-privileges: SHOW GRANTS for users

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,6 @@
+-- Lists all privileges of the MySQL users
+-- These users have not been created yet
+-- Grants for user_0d_1
+SHOW GRANTS FOR user_0d_1@localhost;
+-- Grants for user_0d_2
+SHOW GRANTS FOR user_0d_2@localhost;
