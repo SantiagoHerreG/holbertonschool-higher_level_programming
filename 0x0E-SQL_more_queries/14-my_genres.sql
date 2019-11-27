b7a8c3d34403af6dfb176c83125f755769b9a1f5
SantiagoHerreG <888@holbertonschool.com>
2019-11-27 02:04:41 +0000

    14-my-genres: lists all genres for Dexter show by using double join

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,6 @@
+-- uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
+-- Only prints the genres of Dexter show
+SELECT tv_genres.name
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows ON tv_shows.title = 'Dexter' and tv_show_genres.show_id = tv_shows.id;
