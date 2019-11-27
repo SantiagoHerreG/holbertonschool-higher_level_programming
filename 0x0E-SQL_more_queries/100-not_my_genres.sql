36a512c8d4f0f653490ef578e5ef57d6d0e9aa5d
SantiagoHerreG <888@holbertonschool.com>
2019-11-27 03:35:15 +0000

    100-not_my_genres: returns the genres that are not the categories of Dexter show, using nested selects

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -8,4 +8,5 @@ JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows ON tv_shows.title = 'Dexter' and tv_show_genres.show_id = tv_shows.id
 ORDER BY tv_genres.name) res
 ON tv_genres.name = res.name
-WHERE res.name IS NULL;
+WHERE res.name IS NULL
+ORDER BY tv_genres.name;
