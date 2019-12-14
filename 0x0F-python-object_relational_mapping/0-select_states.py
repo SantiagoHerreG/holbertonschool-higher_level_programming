87a67844693c0988d365727148e503b9d38ab1e2
SantiagoHerreG <888@holbertonschool.com>
2019-12-14 17:13:47 +0000

    Function documentation fixed

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,7 +9,8 @@ if __name__ == "__main__":
     """
 
     db_conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                              passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+                              passwd=sys.argv[2], db=sys.argv[3],
+                              charset="utf8")
     curs = db_conn.cursor()
     curs.execute("SELECT * FROM states ORDER BY states.id ASC")
 
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,9 +8,11 @@ if __name__ == "__main__":
     """The code is not executed when imported
     """
     db_conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                              passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+                              passwd=sys.argv[2], db=sys.argv[3],
+                              charset="utf8")
     curs = db_conn.cursor()
-    curs.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
+    curs.execute("SELECT * FROM states WHERE name LIKE 'N%'\
+ ORDER BY states.id ASC")
 
     query_res = curs.fetchall()
     for row in query_res:
