a218faf332350abb784a8d93ab9d73596516136a
SantiagoHerreG <888@holbertonschool.com>
2019-12-14 17:27:18 +0000

    Try, except added for error checking

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,9 +8,13 @@ import sys
 if __name__ == "__main__":
     """The code is not executed when imported
     """
-    db_conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+    try:
+        db_conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                               passwd=sys.argv[2], db=sys.argv[3],
                               charset="utf8")
+    except:
+        exit(1)
+
     curs = db_conn.cursor()
     curs.execute("SELECT * FROM states WHERE states.name = '{}'\
  ORDER BY states.id ASC".format(sys.argv[4]))
