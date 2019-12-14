164d60e85ce32596beaa43ed67bd0e5f11a3e1b3
SantiagoHerreG <888@holbertonschool.com>
2019-12-14 17:09:39 +0000

    Function documentation fixed

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,6 +5,8 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
+"""The code is not executed when imported
+"""
 
     db_conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                               passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
