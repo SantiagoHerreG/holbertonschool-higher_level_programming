7138eaa9fa4e3b77d39224ae81ad572abd069e10
SantiagoHerreG <888@holbertonschool.com>
2019-12-14 17:06:49 +0000

    2-my_filter_states: filter by states names in a database MySQL from python

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""MySQLdb module for python (MySQL Python) usage
+returns a query where a specified name is searched
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+
+    db_conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                              passwd=sys.argv[2], db=sys.argv[3],
+                              charset="utf8")
+    curs = db_conn.cursor()
+    curs.execute("SELECT * FROM states WHERE states.name = '{}'\
+ ORDER BY states.id ASC".format(sys.argv[4]))
+
+    query_res = curs.fetchall()
+    for row in query_res:
+        print(row)
+    curs.close()
+    db_conn.close()
