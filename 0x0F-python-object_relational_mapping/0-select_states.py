92335069dc4d882e7e5cade5ae53651a9d563207
SantiagoHerreG <888@holbertonschool.com>
2019-12-14 04:09:21 +0000

    0-select states: uses the MySQLdb Python connection for querying a table in a database

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,14 +4,15 @@
 import MySQLdb
 import sys
 
-db_conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                          passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
-curs = db_conn.cursor()
+if __name__ == "__main__":
 
-curs.execute("SELECT * FROM states ORDER BY states.id ASC")
+    db_conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                              passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    curs = db_conn.cursor()
+    curs.execute("SELECT * FROM states ORDER BY states.id ASC")
 
-query_res = curs.fetchall()
-for row in query_res:
-    print(row)
-curs.close()
-db_conn.close()
+    query_res = curs.fetchall()
+    for row in query_res:
+        print(row)
+    curs.close()
+    db_conn.close()
