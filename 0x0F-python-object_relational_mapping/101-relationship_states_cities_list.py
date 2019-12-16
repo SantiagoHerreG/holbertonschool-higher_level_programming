3ab7c539c2f98e31a1a8a022d03a5479cb045f14
SantiagoHerreG <888@holbertonschool.com>
2019-12-16 04:14:37 +0000

    101-prints a list of States and Cities by using a query of State only

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -2,7 +2,7 @@
 """Queries a list of States
 """
 import sys
-from relationship_city import Base, City
+from relationship_city import City
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 from relationship_state import State
@@ -12,14 +12,14 @@ if __name__ == "__main__":
     """
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/\
 {}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for row in session.query(State).join(City).all():
-        print("{}: {}".format(row.id, row.name))
-        for cit in row.cities:
+    query = session.query(State).all()
+    for states in query:
+        print("{}: {}".format(states.id, states.name))
+        for cit in states.cities:
             print("\t{}: {}".format(cit.id, cit.name))
 
     session.close()
