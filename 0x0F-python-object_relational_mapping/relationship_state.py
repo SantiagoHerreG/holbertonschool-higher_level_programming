3d577ca8d59341142990dc6747ba3467fb00d0fa
SantiagoHerreG <888@holbertonschool.com>
2019-12-16 03:57:57 +0000

    class State updated for cascade deletion of children

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -14,4 +14,4 @@ class State(Base):
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
 
-    cities = relationship(City, backref="state", cascade="all, delete-orphan")
+    cities = relationship(City, cascade="all,delete", backref="state")
