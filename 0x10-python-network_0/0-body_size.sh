f3a0ea6ec7bdf77e73d2943da34185387710f02c
SantiagoHerreG <888@holbertonschool.com>
2020-01-06 22:46:19 +0000

    0-body_size printing body size of a response from a server

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Curl a domain (host) and prints the length of the response
-curl -s "$1" -o /dev/null -w "%{size_download}\n"
+# Curl a host and print only the dowloaded bytes
+curl -s 0.0.0.0:5000 -o null -w "%{size_download}\n"
