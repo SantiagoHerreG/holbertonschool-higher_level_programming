3107b30414a6133492aaaba7fcc3004f4ab01886
SantiagoHerreG <888@holbertonschool.com>
2020-01-07 01:53:09 +0000

    6-peak: algorithm for finding recursively a peak in a list

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,30 +3,27 @@
 """
 
 
-def recursion_peak(int_list, idx):
+def recursion_peak(int_list, idx1, idx2):
     """ Recursion for finding a peak
     """
+    idx = int((idx2 - idx1)/2) + idx1
     current = int_list[idx]
-    if idx == 0 or idx == len(int_list) - 1:
+    if idx == idx1 or idx == idx2:
         return current
 
-    if idx + 1 < len(int_list):
-        if (current > int_list[idx + 1] and current > int_list[idx - 1]):
-            return current
-        elif current < int_list[idx - 1]:
-            return recursion_peak(int_list, idx - 1)
-        else:
-            return recursion_peak(int_list, idx + 1)
-    else:
+    if (current > int_list[idx + 1] and current > int_list[idx - 1]):
         return current
+    elif current < int_list[idx - 1]:
+        return recursion_peak(int_list, idx1, idx - 1)
+    else:
+        return recursion_peak(int_list, idx + 1, idx2)
 
 
 def find_peak(list_of_integers):
     """Function that returns a peak in a list of integers
     """
     length = len(list_of_integers)
-    idx = int(length/2)
 
     if (length == 0):
         return None
-    return recursion_peak(list_of_integers, idx)
+    return recursion_peak(list_of_integers, 0, length - 1)
