a7da8e7895df5ade8d2fe0e6ca416f5a5451288f
SantiagoHerreG <888@holbertonschool.com>
2020-01-13 18:46:53 +0000

    7-error_code.py: prints erro codes as responses are not OK

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""takes in a URL, sends a request to the URL and displays the body of
+the response"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    url = sys.argv[1]
+    res = requests.get(url)
+    if res.status_code == requests.codes.ok:
+            print(res.text)
+    else:
+        print("Error code:", res.status_code)
