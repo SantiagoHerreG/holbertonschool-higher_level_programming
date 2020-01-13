7cd8bf5b19c2b18d98ba9a900256b7253d8af75e
SantiagoHerreG <888@holbertonschool.com>
2020-01-13 20:09:54 +0000

    8-json_api.py: uses a Post request to get information from a web site, the returned object is a Json dictionary

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Write a Python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    try:
+        param = sys.argv[1]
+    except:
+        param = ""
+
+    post_param = {"q": param}
+    res = requests.post("http://0.0.0.0:5000/search_user", data=post_param)
+    try:
+        print(res.json()) if len(res.json()) else print("No result")
+    except ValueError:
+        print("Not a valid JSON")
+    except:
+        print("No result")
