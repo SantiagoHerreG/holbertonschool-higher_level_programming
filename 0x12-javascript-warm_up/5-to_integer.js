7b34885386d8e48d602d17b44da314ef85de84d4
SantiagoHerreG <888@holbertonschool.com>
2020-02-04 20:15:56 +0000

    5-to_integer: converts strings to integer Number()

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
 const list = process.argv;
-try {
-  const num = Number(list[2]);
-  if (!num) {
-    console.log('Not a number');
-  } else {
-    console.log('My number: ' + num);
-  }
-} catch {
+const num = Number(list[2]);
+if (!num) {
   console.log('Not a number');
+} else {
+  console.log('My number: ' + num);
 }
