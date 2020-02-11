462db5b411ec4442c49983310476c1a13ed6e20f
SantiagoHerreG <888@holbertonschool.com>
2020-02-11 02:47:05 +0000

    7-occurrences: function that takes a list and a value and returns the number of ocurrences of the value in the list

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  let i = 0;
+  for (; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      count++;
+    }
+  }
+  return count;
+};
