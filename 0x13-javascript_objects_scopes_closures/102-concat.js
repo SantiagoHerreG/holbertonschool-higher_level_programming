ecd60ea3b8e9cb8cf3c3d12c4af8c93f0fa35fd7
SantiagoHerreG <888@holbertonschool.com>
2020-02-11 05:13:41 +0000

    102-concat: takes files as arguments and reads them and prints the read string in a new file (last argument)

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,12 +2,8 @@
 const list = process.argv;
 let i = 2;
 const fs = require('fs');
-let text;
-for (; i < list.length; i++) {
-  try {
-    text = fs.readFileSync(list[i], 'utf-8');
-    process.stdout.write(text);
-  } catch {
-    continue;
-  }
+let text = '';
+for (; i < list.length - 1; i++) {
+  text += fs.readFileSync('./' + list[i], 'utf-8');
 }
+fs.writeFileSync('./' + list[i], text);
