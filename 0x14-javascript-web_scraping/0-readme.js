0c7bb0ec7488fe131677658cc04f110fced142a1
SantiagoHerreG <888@holbertonschool.com>
2020-02-11 16:48:54 +0000

    0-readme reads a file and prints its content

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const list = process.argv;
+const fs = require('fs');
+try {
+  process.stdout.write(fs.readFileSync('./' + list[2], 'utf-8'));
+} catch (error) {
+  console.log(error);
+}
