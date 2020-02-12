e1e09b2de4927132657541988165729d541c858b
SantiagoHerreG <888@holbertonschool.com>
2020-02-12 03:18:28 +0000

    4-starwars_count: prints the count of movies in which the character ID 18 is present

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,8 +1,22 @@
 #!/usr/bin/node
+const list = process.argv;
 const request = require('request');
-request('https://swapi.co/api/people/18/', function (error, response, body) {
+request(list[2], function (error, response, body) {
   if (error) {
     console.error('error:', error);
   }
-  console.log(JSON.parse(body).films.length);
+  let i = 0;
+  let j;
+  let count = 0;
+  let actors;
+  const movies = JSON.parse(body).results;
+  for (; i < movies.length; i++) {
+    actors = movies[i].characters;
+    for (j = 0; j < actors.length; j++) {
+      if (actors[j] === 'https://swapi.co/api/people/18/') {
+        count++;
+      }
+    }
+  }
+  console.log(count);
 });
