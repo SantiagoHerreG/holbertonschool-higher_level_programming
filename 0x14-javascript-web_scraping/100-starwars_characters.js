68706a62ef14d075a1b5cdb9834b8df0510f3cad
SantiagoHerreG <888@holbertonschool.com>
2020-02-12 04:15:20 +0000

    100-starwars_characters: double request handled for getting the movie and then requesting for each character

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const list = process.argv;
+const request = require('request');
+request('https://swapi.co/api/films/' + list[2], function (error, response, body) {
+  if (error) {
+    console.error('error:', error);
+  }
+  let i = 0;
+  const actors = JSON.parse(body).characters;
+  for (i = 0; i < actors.length; i++) {
+    request(actors[i], function (error, response, body) {
+      if (error) {
+        console.error('error:', error);
+      }
+      console.log(JSON.parse(body).name);
+    });
+  }
+});
